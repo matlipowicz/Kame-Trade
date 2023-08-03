2e7e6f652f1cddb23e58f10f0628945f0bc57b84
Mateusz Lipowicz <lipusek1997@gmail.com>
2023-08-03 23:47:12 +0200

    Commenting unused parameters and components pt.5

--- src/components/UnderConstruction/Construction.tsx
+++ src/components/UnderConstruction/Construction.tsx
@@ -1,12 +1,11 @@
 import { Image, Box, GridItem } from "@chakra-ui/react";
-import img from "./assets/miscellaneous/Under-construction.png";
 
 export const Construction = () => {
     return (
         <>
             <GridItem colSpan={2}>
                 <Box as="picture" w="100%" display="flex" justifyContent="center">
-                    <Image src={img} alt="Under-Construction-img" w="60%" />
+                    <Image src="./assets/miscellaneous/Under-construction.png" alt="Under-Construction-img" w="60%" />
                 </Box>
             </GridItem>
         </>
